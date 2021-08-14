4ff144ef501b5faf3f7b6c7a5b539dc39ef1cda9
YutaroMatsumoto <spurs03170629@gmail.com>
2021-08-14 11:30:03 +0900

    ♻️ ファイル構成修正

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div className="font-black">aiueo</div>
+    </div>
+  );
+};
+
+export default Home;
