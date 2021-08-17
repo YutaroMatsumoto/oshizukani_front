4f7b4b7b7aa47bdd9fd12fa991caa4da00cb7382
YutaroMatsumoto <spurs03170629@gmail.com>
2021-08-17 22:26:31 +0900

    💄 Headerデザイン修正

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,8 +1,19 @@
+import { Button } from "src/components/molecules/Button";
+
 export const Header = () => {
+  const auth = false; // 暫定対応
+
   return (
-    <header className="flex py-4 w-full h-16 bg-white fixed">
-      <div className="ml-10 w-8 rounded bg-yellow"></div>
-      <div className="font-black">aiueo</div>
+    <header className="flex py-4 w-full h-16 fixed bg-white">
+      <div
+        className="flex items-center ml-10 font-black text-green-400 cursor-pointer"
+        onClick={() => console.log("Click Logo")}
+      >
+        oshizukani
+      </div>
+      <div className="flex items-center ml-auto mr-10">
+        <Button auth={auth} />
+      </div>
     </header>
   );
 };
