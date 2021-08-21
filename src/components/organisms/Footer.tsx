cd5e83e1310e6a8d28983433f90bfcc959c35385
YutaroMatsumoto <spurs03170629@gmail.com>
2021-08-21 10:08:06 +0900

    💄 [add] Footer

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,11 @@
+import { Button } from "src/components/molecules/Button";
+
+export const Footer = () => {
+  const auth = false; // 暫定対応
+
+  return (
+    <footer className="bottom-0 flex items-center justify-center w-full h-12 bg-white">
+      fotterです
+    </footer>
+  );
+};
