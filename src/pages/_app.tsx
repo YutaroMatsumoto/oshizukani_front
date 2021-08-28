64e095525cc6939d111508740475dda2f9f0ac63
YutaroMatsumoto <spurs03170629@gmail.com>
2021-08-28 09:51:12 +0900

    ➕ twin.macroの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "src/styles/globals.css";
+import type { AppProps } from "next/app";
+import { GlobalStyles } from "twin.macro";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-full">
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
