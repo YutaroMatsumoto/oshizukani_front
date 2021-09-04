2e79b0a2c918e2ec2dfd6270de0a50880bf49f2f
YutaroMatsumoto <spurs03170629@gmail.com>
2021-09-04 10:10:52 +0900

    💄 modify: TabItemデザイン修正（アンダーバー追加）

--- src/components/molecules/TabItem.tsx
+++ src/components/molecules/TabItem.tsx
@@ -11,7 +11,10 @@ interface Props {
 export const TabItem = ({ content, href, active }: Props) => {
   return (
     <Link href={href}>
-      <div css={[active && tw`text-green-400`]}>{content}</div>
+      <div>
+        <div css={[active && tw`text-green-400`]}>{content}</div>
+        <div tw="h-1 rounded" css={[active && tw`bg-green-400`]}></div>
+      </div>
     </Link>
   );
 };
