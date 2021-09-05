ff1c8f7745f3e9f7d8226358028291f30241d983
YutaroMatsumoto <spurs03170629@gmail.com>
2021-09-05 20:44:45 +0900

    ♻️ modify: Buttonを整理

--- src/components/molecules/Button.tsx
+++ src/components/molecules/Button.tsx
@@ -4,24 +4,17 @@ interface Props {
   width?: string;
   height?: string;
   color?: boolean;
-  auth: boolean;
+  text: string;
   onClick: () => void;
 }
 
-export const Button = ({ auth, color, width, height, onClick }: Props) => {
+export const Button = ({ color, width, height, text, onClick }: Props) => {
   return (
-    // <button tw="w-20 h-9 rounded-lg border-2 border-white bg-green-400 ">
-    //   <span tw="font-black text-white">
-    //     {auth ? "ログアウト" : "ログイン"}
-    //   </span>
-    // </button>
     <button
       tw="w-20 h-9 rounded-lg border-2 border-green-400 bg-white"
       onClick={onClick}
     >
-      <span tw="font-black text-green-400">
-        {auth ? "ログアウト" : "ログイン"}
-      </span>
+      <span tw="font-black text-green-400">{text}</span>
     </button>
   );
 };
