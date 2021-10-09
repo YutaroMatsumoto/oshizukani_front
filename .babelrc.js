40cfc5a2ab6b1d57a4e5e92831bc5aefc6978d78
YutaroMatsumoto <spurs03170629@gmail.com>
2021-10-09 20:11:12 +0900

    💄 ➕ Global CSS cannot be imoprted ...のエラー解消 とりあえずカレンダー表示

--- .babelrc.js
+++ .babelrc.js
@@ -11,5 +11,18 @@ module.exports = {
     ],
   ],
   plugins: ["@emotion/babel-plugin", "babel-plugin-macros"],
+  overrides: [
+    {
+      include: ["./node_modules"],
+      plugins: [
+        [
+          "babel-plugin-transform-require-ignore",
+          {
+            extensions: [".css"],
+          },
+        ],
+      ],
+    },
+  ],
   // plugins: ["babel-plugin-macros", ["styled-components", { ssr: true }]],
 };
--- src/pages/day.tsx
+++ src/pages/day.tsx
@@ -1,8 +1,18 @@
 import "twin.macro";
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import "@fullcalendar/common/main.css";
+import "@fullcalendar/daygrid/main.css";
+
 import { BaseTemplate } from "src/components/templates/BaseTemplate";
 
 const Day = () => {
-  return <BaseTemplate> </BaseTemplate>;
+  return (
+    <BaseTemplate>
+      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+    </BaseTemplate>
+  );
 };
 
 export default Day;
