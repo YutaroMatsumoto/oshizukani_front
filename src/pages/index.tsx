11edce24ceaf2884d2b98fdc51fe842b06280420
YutaroMatsumoto <spurs03170629@gmail.com>
2021-11-27 11:21:02 +0900

    💄 各UIへの分岐対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 import "twin.macro";
-import type { NextPage } from "next";
-import { Children, useState } from "react";
-import { BaseTemplate } from "src/components/templates/BaseTemplate";
-import { NowMtg } from "src/components/molecules/NowMtg";
+import { useState } from "react";
+import { NowMtg } from "src/components/molecules/Home/NowMtg";
+import { SuggestUserRegistration } from "src/components/molecules/Home/SuggestUserRegistration";
+import { SuggestApiLink } from "src/components/molecules/Home/SuggestApiLink";
 
 const Home = () => {
-  const [isMtg, setIsMtg] = useState(true);
-  console.log(isMtg);
-  return (
-    <BaseTemplate>{isMtg ? <NowMtg /> : <div>あいうえお</div>}</BaseTemplate>
-  );
+  const [isUserRegistered, setIsUserRegistered] = useState<boolean>(true);
+  const [isApiLinked, setIsApiLinked] = useState<boolean>(false);
+  const [isMtg, setIsMtg] = useState<boolean>(true);
+
+  if (isUserRegistered) {
+    if (isApiLinked) {
+      return <NowMtg isMtg={isMtg} />;
+    } else {
+      return <SuggestApiLink />;
+    }
+  } else {
+    return <SuggestUserRegistration />;
+  }
 };
 
 export default Home;
