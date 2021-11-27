709bae0f2b3302025107444722b46e97e459e178
YutaroMatsumoto <spurs03170629@gmail.com>
2021-11-27 11:22:11 +0900

    fix: デフォルト表示の画面を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { SuggestApiLink } from "src/components/molecules/Home/SuggestApiLink";
 
 const Home = () => {
   const [isUserRegistered, setIsUserRegistered] = useState<boolean>(true);
-  const [isApiLinked, setIsApiLinked] = useState<boolean>(false);
+  const [isApiLinked, setIsApiLinked] = useState<boolean>(true);
   const [isMtg, setIsMtg] = useState<boolean>(true);
 
   if (isUserRegistered) {
