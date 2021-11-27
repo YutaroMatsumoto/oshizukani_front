8545375a10e3ab6de379ba35a6212a59d07a75cd
YutaroMatsumoto <spurs03170629@gmail.com>
2021-11-27 10:57:38 +0900

    💄 BaseTemplateの修正に伴うNowMtgの修正

--- src/components/molecules/NowMtg.tsx
+++ src/components/molecules/NowMtg.tsx
@@ -7,12 +7,10 @@ interface Props {
 
 export const NowMtg = () => {
   return (
-    <div tw="h-full flex justify-center items-center text-center">
-      <div tw="flex justify-center items-center w-80 h-80 border-4 border-solid border-green-400 rounded-full ">
-        <div tw="leading-7 text-2xl font-bold text-green-400 ">
-          <p>現在会議中です。</p>
-          <p>お静かに。</p>
-        </div>
+    <div tw="flex justify-center items-center text-center w-80 h-80 border-4 border-solid border-green-400 rounded-full">
+      <div tw="leading-7 text-2xl font-bold text-green-400 ">
+        <p>現在会議中です。</p>
+        <p>お静かに。</p>
       </div>
     </div>
   );
