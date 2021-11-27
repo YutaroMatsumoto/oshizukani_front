1778bf3a1bdab5b9f733cbd8cea08b3354ef334e
YutaroMatsumoto <spurs03170629@gmail.com>
2021-11-27 10:48:58 +0900

    💄 Header修正

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -16,29 +16,31 @@ export const Header = () => {
   const showMember = useCallback(() => modalDispatch({ type: "member" }), []);
 
   return (
-    <header tw="flex items-center py-4 w-full h-16 fixed bg-white z-10">
+    <header tw="flex items-center py-4 px-14 w-full h-16 fixed bg-white z-10">
       <div
-        tw="flex items-center ml-10 font-black text-green-400 cursor-pointer"
+        tw="flex items-center font-black text-green-400 cursor-pointer"
         onClick={() => console.log("Click Logo")}
       >
         oshizukani
       </div>
-      <div tw="mx-auto">
+      {/* TODO: 将来的に必要になったら入れる */}
+      {/* <div tw="mx-auto">
         <Tab />
-      </div>
-      <div tw=""></div>
-      <div tw="flex items-center mr-10 ml-5 cursor-pointer">
-        {auth ? (
-          <IconButton
-            type={IconType.SETTING}
-            color="#065f46"
-            size={25}
-            onClick={showMember}
-          />
-        ) : (
-          <Button text="ログイン" onClick={() => console.log("Login")} />
-        )}
-        {context.visible && <Modal />}
+      </div> */}
+      <div tw="ml-auto">
+        <div tw="flex items-center cursor-pointer">
+          {auth ? (
+            <IconButton
+              type={IconType.SETTING}
+              color="#065f46"
+              size={25}
+              onClick={showMember}
+            />
+          ) : (
+            <Button text="ログイン" onClick={() => console.log("Login")} />
+          )}
+          {context.visible && <Modal />}
+        </div>
       </div>
     </header>
   );
