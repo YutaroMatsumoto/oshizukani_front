eb27132870da8ec1a5212f75b2d7b250181b5e25
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-04 10:06:35 +0900

    💄 textinputのデザイン作成

---
+++ src/components/molecules/Form/TextInput.tsx
@@ -0,0 +1,7 @@
+import "twin.macro";
+
+export const TextInput = () => {
+  return (
+    <input tw="border-solid border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:border focus:border-green-200" />
+  );
+};
