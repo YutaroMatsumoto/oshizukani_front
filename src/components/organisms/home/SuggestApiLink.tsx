df7fa587f4a9292baeca0a1b5c09ee32567cbfe8
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-06 18:20:07 +0900

    fix: SuggestApiLink

--- src/components/organisms/home/SuggestApiLink.tsx
+++ src/components/organisms/home/SuggestApiLink.tsx
@@ -1,12 +1,10 @@
 import "twin.macro";
-import { TextInput } from "src/components/molecules/form/TextInput";
 import { BaseTemplate } from "src/components/templates/BaseTemplate";
 
 export const SuggestApiLink = () => {
   return (
     <BaseTemplate>
       <p>！APIが連携されていません！</p>
-      <TextInput />
     </BaseTemplate>
   );
 };
