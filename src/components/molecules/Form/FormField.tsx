46fb6fde4b43de3b9ed234f945a11033bdbbfd4d
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-06 18:19:44 +0900

    💄 FormField作成

---
+++ src/components/molecules/Form/FormField.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'twin.macro';
+
+interface Props {
+  label: string;
+  className?: string;
+}
+
+export const FormField = ({
+  label,
+  className,
+  children,
+}: React.PropsWithChildren<Props>) => {
+  return (
+    <div tw="flex items-center" className={className}>
+      <label tw="font-bold w-28">{label}</label>
+      <div tw="ml-4 w-full">{children}</div>
+    </div>
+  );
+};
