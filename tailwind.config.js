181aef679e1c8788db380cb93e6a0015f8f03157
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-06 18:05:59 +0900

    fix: tailwind.config 拡張

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        100: '25rem',
+        125: '31.25rem',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
