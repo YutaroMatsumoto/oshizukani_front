d36ce9956393f16a6610335c14cf1b3c4de103bf
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-06 18:41:42 +0900

    💄 Buttonをアップデート

--- src/components/molecules/Button.tsx
+++ src/components/molecules/Button.tsx
@@ -1,19 +1,32 @@
-import "twin.macro";
+import tw from 'twin.macro';
 
 interface Props {
   className?: string;
   title: string;
+  icon?: React.ReactNode;
+  black?: boolean;
   onClick: () => void;
 }
 
-export const Button = ({ className, title, onClick }: Props) => {
+export const Button = ({ className, title, icon, black, onClick }: Props) => {
   return (
     <button
-      tw="w-20 h-9 rounded-lg border-2 border-green-400 bg-white"
+      tw="flex items-center h-9 px-5 rounded-lg border-2  bg-white"
+      css={[black ? tw`border-black` : tw`border-green-400`]}
       className={className}
       onClick={onClick}
     >
-      <span tw="font-black text-green-400">{title}</span>
+      {icon && (
+        <div
+          tw="fill-current mr-2 text-green-400"
+          css={[black ? tw`text-black` : tw`text-green-400`]}
+        >
+          {icon}
+        </div>
+      )}
+      <span tw="font-black " css={[!black && tw`text-green-400`]}>
+        {title}
+      </span>
     </button>
   );
 };
