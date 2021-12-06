fd07d3ee733bdc40bff88d92f27e1f0478f0160f
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-06 18:18:06 +0900

    update: TextInput

--- src/components/molecules/Form/TextInput.tsx
+++ src/components/molecules/Form/TextInput.tsx
@@ -1,7 +1,16 @@
-import "twin.macro";
+import 'twin.macro';
 
-export const TextInput = () => {
+interface Props {
+  value: string;
+  onChange: () => void;
+}
+
+export const TextInput = ({ value, onChange }: Props) => {
   return (
-    <input tw="border-solid border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:border focus:border-green-200" />
+    <input
+      tw="w-full border-solid border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:border focus:border-green-200"
+      value={value}
+      onChange={onChange}
+    />
   );
 };
