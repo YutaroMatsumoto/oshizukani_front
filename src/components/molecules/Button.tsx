6e1a14a370d939686067087fd3e02fa2646f0896
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-06 16:07:05 +0900

    fix: Buttonのprops修正

--- src/components/molecules/Button.tsx
+++ src/components/molecules/Button.tsx
@@ -4,17 +4,17 @@ interface Props {
   width?: string;
   height?: string;
   color?: boolean;
-  text: string;
+  title: string;
   onClick: () => void;
 }
 
-export const Button = ({ color, width, height, text, onClick }: Props) => {
+export const Button = ({ color, width, height, title, onClick }: Props) => {
   return (
     <button
       tw="w-20 h-9 rounded-lg border-2 border-green-400 bg-white"
       onClick={onClick}
     >
-      <span tw="font-black text-green-400">{text}</span>
+      <span tw="font-black text-green-400">{title}</span>
     </button>
   );
 };
