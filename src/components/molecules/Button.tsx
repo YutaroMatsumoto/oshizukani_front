7d969cff1642dc9c749614a82a06a88d802d04b9
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-06 18:17:17 +0900

    fix: Button

--- src/components/molecules/Button.tsx
+++ src/components/molecules/Button.tsx
@@ -1,17 +1,16 @@
 import "twin.macro";
 
 interface Props {
-  width?: string;
-  height?: string;
-  color?: boolean;
+  className?: string;
   title: string;
   onClick: () => void;
 }
 
-export const Button = ({ color, width, height, title, onClick }: Props) => {
+export const Button = ({ className, title, onClick }: Props) => {
   return (
     <button
       tw="w-20 h-9 rounded-lg border-2 border-green-400 bg-white"
+      className={className}
       onClick={onClick}
     >
       <span tw="font-black text-green-400">{title}</span>
