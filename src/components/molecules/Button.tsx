91c3b036baf2301773b128d7d2762527b20f5918
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-07 10:32:49 +0900

    💄 hoverするとButtonの色が変わるよう修正

--- src/components/molecules/Button.tsx
+++ src/components/molecules/Button.tsx
@@ -1,4 +1,5 @@
 import tw from 'twin.macro';
+import { useState } from 'react';
 
 interface Props {
   className?: string;
@@ -9,22 +10,39 @@ interface Props {
 }
 
 export const Button = ({ className, title, icon, black, onClick }: Props) => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
   return (
     <button
       tw="flex items-center h-9 px-5 rounded-lg border-2  bg-white"
-      css={[black ? tw`border-black` : tw`border-green-400`]}
+      css={[
+        black ? tw`border-black ` : tw`border-green-400`,
+        isHover && black && tw`transition bg-black`,
+        isHover && !black && tw`transition bg-green-400`,
+      ]}
       className={className}
       onClick={onClick}
+      onMouseOver={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
       {icon && (
         <div
           tw="fill-current mr-2 text-green-400"
-          css={[black ? tw`text-black` : tw`text-green-400`]}
+          css={[
+            black ? tw`text-black` : tw`text-green-400`,
+            isHover && tw`transition text-white`,
+          ]}
         >
           {icon}
         </div>
       )}
-      <span tw="font-black " css={[!black && tw`text-green-400`]}>
+      <span
+        tw="font-black"
+        css={[
+          !black && tw`text-green-400`,
+          isHover && tw`transition text-white`,
+        ]}
+      >
         {title}
       </span>
     </button>
