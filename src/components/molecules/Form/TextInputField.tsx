be963f3ba222775c15aa8f1d150307d19c8d7338
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-07 22:40:24 +0900

    fix: buildエラーテスト

--- src/components/molecules/Form/TextInputField.tsx
+++ src/components/molecules/Form/TextInputField.tsx
@@ -9,8 +9,8 @@ export const TextInputField = ({ value, onChange }: Props) => {
   return (
     <input
       tw="w-full border-solid border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:border focus:border-green-200"
-      value={value}
-      onChange={onChange}
+      // value={value}
+      // onChange={onChange}
     />
   );
 };
