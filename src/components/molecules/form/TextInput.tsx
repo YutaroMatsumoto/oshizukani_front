249d4b7dafb905a066d834a97db562654dfdc505
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-11 10:21:22 +0900

    update: TextInputにclassName propsを追加

--- src/components/molecules/form/TextInput.tsx
+++ src/components/molecules/form/TextInput.tsx
@@ -2,13 +2,15 @@ import 'twin.macro';
 
 interface Props {
   value: string;
+  className?: string;
   onChange: () => void;
 }
 
-export const TextInput = ({ value, onChange }: Props) => {
+export const TextInput = ({ value, className, onChange }: Props) => {
   return (
     <input
       tw="w-full border-solid border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:border focus:border-green-200"
+      className={className}
       value={value}
       onChange={onChange}
     />
