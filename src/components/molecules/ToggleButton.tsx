f7312a596855bf6de2464a6fa320222f528e5884
YutaroMatsumoto <spurs03170629@gmail.com>
2021-12-12 12:13:52 +0900

    💄 ToggleButton実装

---
+++ src/components/molecules/ToggleButton.tsx
@@ -0,0 +1,34 @@
+import tw, { css } from 'twin.macro';
+
+type Props = {
+  toggleOn: boolean;
+  onClick: () => void;
+};
+
+// 単純なButtonであり、formではない。今後問題ないか検討。
+// toggleのstateは呼び出し側で管理する
+export const ToggleButton = ({ toggleOn, onClick }: Props) => {
+  return (
+    <button onClick={onClick}>
+      <div tw="cursor-pointer">
+        <div
+          tw="items-center w-12 p-1 bg-green-400 rounded-3xl transition"
+          css={[toggleOn && tw`transition bg-gray-200`]}
+        >
+          <div
+            tw="w-4 h-4 bg-white rounded-3xl transition-transform ease-in-out"
+            css={[
+              toggleOn
+                ? css`
+                    transform: translateX(0px) translateY(0px);
+                  `
+                : css`
+                    transform: translateX(24px) translateY(0px);
+                  `,
+            ]}
+          />
+        </div>
+      </div>
+    </button>
+  );
+};
