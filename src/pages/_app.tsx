a8cb3aaa72eb1eedc858b712821ed4b936d98997
YutaroMatsumoto <spurs03170629@gmail.com>
2022-01-09 20:10:22 +0900

    テストプッシュ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import 'src/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { GlobalStyles } from 'twin.macro';
 import { ModalContextProvider } from 'src/contexts/ModalContextProvider';
-import { GoogleApi } from 'src/components/googleApi';
+import { GoogleApi } from 'src/components/GogleApi';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
