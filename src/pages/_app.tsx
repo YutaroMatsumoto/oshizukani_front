7acd866262af28c4c6f8a6251e2cd8192d0e28dd
YutaroMatsumoto <spurs03170629@gmail.com>
2022-01-09 16:21:25 +0900

    add: _app.tsxでGoogleApiコンポーネントを読み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
-import "twin.macro";
-import "src/styles/globals.css";
-import type { AppProps } from "next/app";
-import { GlobalStyles } from "twin.macro";
-import { ModalContextProvider } from "src/contexts/ModalContextProvider";
+import 'twin.macro';
+import 'src/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { GlobalStyles } from 'twin.macro';
+import { ModalContextProvider } from 'src/contexts/ModalContextProvider';
+import { GoogleApi } from 'src/components/googleApi';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div tw="h-full">
       <ModalContextProvider>
+        <GoogleApi />
         <Component {...pageProps} />
         <GlobalStyles />
       </ModalContextProvider>
