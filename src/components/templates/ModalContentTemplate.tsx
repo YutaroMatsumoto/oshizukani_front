259d568aee483ce51fd6691e4feef4bcdaee0129
YutaroMatsumoto <spurs03170629@gmail.com>
2022-04-22 18:09:06 +0900

    💄 利用できない旨の注意書きを表示できるよう修正（モーダル）

--- src/components/templates/ModalContentTemplate.tsx
+++ src/components/templates/ModalContentTemplate.tsx
@@ -1,20 +1,29 @@
-import 'twin.macro';
-import React from 'react';
-import { Divider } from 'src/components/molecules/Divider';
+import 'twin.macro'
+import React from 'react'
+import { Divider } from 'src/components/molecules/Divider'
 
 interface Props {
-  title?: string;
+  title?: string
+  unavailable?: boolean
 }
 
 export const ModalContentTemplate = ({
   title,
+  unavailable = false,
   children,
 }: React.PropsWithChildren<Props>) => {
   return (
     <>
-      <div tw="px-2 font-semibold">{title}</div>
+      <div tw="px-2 font-semibold">
+        {title}
+        {unavailable && (
+          <span tw="ml-4 text-red">
+            ※こちらの機能は現在ご利用いただけません。
+          </span>
+        )}
+      </div>
       <Divider />
       <div tw="h-124 mt-6">{children}</div>
     </>
-  );
-};
+  )
+}
