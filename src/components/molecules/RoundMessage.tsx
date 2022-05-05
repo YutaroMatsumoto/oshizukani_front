c6e3a972a0317a2d535de22061b4a38cbb365684
YutaroMatsumoto <spurs03170629@gmail.com>
2022-05-05 10:50:56 +0900

    💄 RoundMessage Component作成

---
+++ src/components/molecules/RoundMessage.tsx
@@ -0,0 +1,29 @@
+import tw from 'twin.macro'
+
+interface Props {
+  className?: string
+  message: string
+  subMessage: string
+  isError?: boolean
+}
+
+export const RoundMessage = ({
+  message,
+  subMessage,
+  isError = false,
+}: Props) => {
+  return (
+    <div
+      tw="flex justify-center items-center text-center w-80 h-80 border-4 border-solid rounded-full"
+      css={[isError ? tw`border-red` : tw`border-lightGreen`]}
+    >
+      <div
+        tw="leading-7 font-bold"
+        css={[isError ? tw`text-xl text-red ` : tw`text-2xl text-lightGreen`]}
+      >
+        <p>{message}</p>
+        <p>{subMessage}</p>
+      </div>
+    </div>
+  )
+}
