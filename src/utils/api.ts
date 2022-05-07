c30d5a5bb33282c65fd4e8a3d2b6ecc98cead04c
YutaroMatsumoto <spurs03170629@gmail.com>
2022-05-07 18:10:31 +0900

    ♻️ 不要な変数を削除

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -8,9 +8,6 @@ type IsMtg = {
   isMtg: boolean
 }
 
-const local = 'http://0.0.0.0/api'
-const server = 'https://oshizukani.herokuapp.com/api'
-
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: 5000,
